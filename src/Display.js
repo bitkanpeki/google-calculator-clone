70821d7b3be6923c615e8732bb8aa23ccca6c156
Jonathan Boulay <jonathanboulay@outlook.com>
2020-06-17 15:55:45 +0900

    refactor the superscript nesting function with one recursive function

--- src/Display.js
+++ src/Display.js
@@ -1,77 +1,109 @@
 import React from 'react'
+import { v4 as uuid } from 'uuid'
 import { isNumeric } from './utilities'
+
+const addSpaces = (expression, index) => {
+  const value = expression[index]
+
+  if (/^[+\-×÷]$/.test(value)) return ` ${value} `
+
+  if (
+    expression[index - 1] !== '(' &&
+    (value === 'sin' ||
+      value === 'cos' ||
+      value === 'tan' ||
+      value === 'arcsin' ||
+      value === 'arccos' ||
+      value === 'arctan' ||
+      value === '√' ||
+      value === 'ln' ||
+      value === 'log')
+  )
+    return ` ${value}`
+
+  return value
+}
 
 const nestSuperscript = (expression) => {
   let superscriptNestedArray = []
+  let arrayClosingParentheses = []
 
   for (let index = 0; index < expression.length; index++) {
-    if (expression[index] === '^') {
-      let superscriptEnd = expression.length
-
-      for (let indexSup = index; indexSup < superscriptEnd; indexSup++) {
-        if (
-          !isNumeric(expression[indexSup]) &&
-          expression[indexSup] !== '!' &&
-          expression[indexSup] !== '%' &&
-          expression[indexSup] !== 'E' &&
-          expression[indexSup] !== '^' &&
-          expression[indexSup] !== '('
-        ) {
-          superscriptEnd = indexSup
-          break
-        }
-      }
-
-      let superscriptSubExpression = expression.slice(index, superscriptEnd)
-
-      superscriptSubExpression.length === 1
-        ? (superscriptSubExpression[0] = (
-            <span style={{ color: 'rgb(204, 204, 204)' }}>□</span>
-          ))
-        : (superscriptSubExpression[0] = (
-            <span style={{ fontSize: '0px' }}>□</span>
-          ))
-
-      index += superscriptSubExpression.length
-
-      superscriptNestedArray.push(
-        <sup>{nestSuperscript(superscriptSubExpression)}</sup>
+    if (expression[index] === '(') {
+      arrayClosingParentheses.push(
+        <span key={uuid()} style={{ color: 'rgb(204, 204, 204)' }}>
+          )
+        </span>
       )
     }
 
-    superscriptNestedArray.push(expression[index])
+    if (expression[index] === ')') arrayClosingParentheses.pop()
+
+    if (expression[index] === '^') {
+      const slicedExp = expression.slice(index)
+
+      let superscriptEnd
+
+      if (slicedExp[1] === '(') {
+        let openedParentheses = 0
+
+        for (const [index, value] of slicedExp.entries()) {
+          if (value === '(') openedParentheses += 1
+
+          if (value === ')') {
+            openedParentheses -= 1
+
+            if (openedParentheses === 0) {
+              superscriptEnd = index + 1
+              break
+            }
+          }
+        }
+      } else {
+        superscriptEnd = slicedExp.findIndex((element) => {
+          return (
+            !isNumeric(element) &&
+            element !== '!' &&
+            element !== '%' &&
+            element !== 'E' &&
+            element !== '^'
+          )
+        })
+      }
+
+      if (superscriptEnd === -1) superscriptEnd = slicedExp.length
+
+      let superscriptSubExpression = slicedExp.slice(0, superscriptEnd)
+
+      if (superscriptSubExpression.length === 1) {
+        superscriptSubExpression[0] = (
+          <span key={uuid()} style={{ color: 'rgb(204, 204, 204)' }}>
+            □
+          </span>
+        )
+      } else {
+        superscriptSubExpression[0] = (
+          <span key={uuid()} style={{ fontSize: '0px' }}>
+            □
+          </span>
+        )
+      }
+
+      index += superscriptSubExpression.length - 1
+
+      superscriptNestedArray.push(
+        <sup key={uuid()}>{nestSuperscript(superscriptSubExpression)}</sup>
+      )
+
+      continue
+    }
+
+    superscriptNestedArray.push(addSpaces(expression, index))
   }
 
-  return superscriptNestedArray
+  return superscriptNestedArray.concat(arrayClosingParentheses)
 }
 
-const addSpaces = (expression) => {
-  return expression.map((value, index) => {
-    if (/^[+\-×÷]$/.test(value)) return ` ${value} `
-
-    if (
-      expression[index - 1] !== '(' &&
-      (value === 'sin' ||
-        value === 'cos' ||
-        value === 'tan' ||
-        value === 'arcsin' ||
-        value === 'arccos' ||
-        value === 'arctan' ||
-        value === '√' ||
-        value === 'ln' ||
-        value === 'log')
-    )
-      return ` ${value}`
-
-    return value
-  })
-}
-
-const Display = ({ expression }) => {
-  const expressionNestedSuperscript = nestSuperscript(expression),
-    expressionNestedWithSpaces = addSpaces(expressionNestedSuperscript)
-
-  return <>{expressionNestedWithSpaces}</>
-}
+const Display = ({ expression }) => nestSuperscript(expression)
 
 export default Display
